ef9465cb8d4e497415b7c08284b9f14bdc7995da
Max Lyapkalo <m.lyapkalo@gmail.com>
2018-05-12 16:52:34 +0300

    Created DI module with Ninject. Bound IUnitOfWorkIdentity to IdentityUnitOfWork implem.

---
+++ BLL/Infrastructure/BindingModule.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Identity.Interfaces;
+using DAL.Identity.Repositories;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Infrastructure
+{
+    public class BindingModule : NinjectModule
+    {
+        private string connectionString;
+
+        public BindingModule(string connection)
+        {
+            connectionString = connection;
+        }
+
+        public override void Load()
+        {
+            Bind<IUnitOfWorkIdentity>().To<IdentityUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
