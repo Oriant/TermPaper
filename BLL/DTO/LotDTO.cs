a66eff0eafe7241564f48b519256dc3b3a1cb2fe
Max Lyapkalo <m.lyapkalo@gmail.com>
2018-05-15 14:14:54 +0300

    Created DTO for Category and Lot

--- BLL/DTO/LotDTO.cs
+++ BLL/DTO/LotDTO.cs
@@ -8,5 +8,24 @@ namespace BLL.DTO
 {
     public class LotDTO
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsConfirmed { get; set; }
+
+        public ICollection<CategoryDTO> Categories { get; set; }
+
+        public UserDTO User { get; set; }
+
+        public LotDTO()
+        {
+            Categories = new List<CategoryDTO>();
+            IsConfirmed = false;
+        }
     }
 }
