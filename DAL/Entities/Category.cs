c039be1cb0da616462361e5afc31a9c5f7b67243
Max Lyapkalo <m.lyapkalo@gmail.com>
2018-05-15 13:11:26 +0300

    Added entities: Lot, Category

---
+++ DAL/Entities/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Lot> RelatedLots { get; set; }
+
+        public Category()
+        {
+            RelatedLots = new List<Lot>();
+        }
+    }
+}
---
+++ DAL/Entities/Lot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Lot
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsConfirmed { get; set; }
+
+        public virtual ICollection<Category> Categories { get; set; }
+
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public Lot()
+        {
+            IsConfirmed = false;
+            Categories = new List<Category>();
+
+        }
+    }
+}
