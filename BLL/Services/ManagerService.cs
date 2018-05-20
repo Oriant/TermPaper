912a96d0a5418f403c7d050c0cb9533696a1e4fe
Max Lyapkalo <m.lyapkalo@gmail.com>
2018-05-20 15:26:16 +0300

    Added interface and implementation of Manager service to BLL

---
+++ BLL/Services/ManagerService.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using DAL.Entities;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class ManagerService : IManagerService
+    {
+        IUnitOfWork Database { get; set; }
+
+        public ManagerService(IUnitOfWork unitOfWork)
+        {
+            Database = unitOfWork;
+        }
+
+        public void ConfirmLot(LotDTO lotDto)
+        {
+            lotDto.IsConfirmed = true;
+            var lot = Mapper.Map<Lot>(lotDto);
+
+            Database.Lots.Update(lot);
+        }
+
+        public IEnumerable<LotDTO> GetUnconfirmedLots()
+        {
+            return Mapper.Map<IEnumerable<Lot>, IEnumerable<LotDTO>>
+                (Database.Lots
+                .GetAll()
+                .Where(x => !x.IsConfirmed));
+        }
+    }
+}
