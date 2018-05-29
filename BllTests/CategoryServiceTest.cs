73b552ac7ec5d4fce8b33267c9df717f1e016f5f
Frozen <kiril.kohan@gmail.com>
2018-05-29 15:07:49 +0300

    InitialCommit

---
+++ BllTests/CategoryServiceTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using BLL.Infrastructure;
+using BLL.Interfaces;
+using BLL.Services;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+
+namespace BllTests
+{
+	[TestClass]
+	public class CategoryServiceTest
+	{
+		static CategoryServiceTest()
+		{
+			try
+			{
+				Mapper.Initialize(cfg =>
+				MapperConfig.Initialize()
+				);
+			}
+			catch { }
+		}
+		
+		/*
+		[TestMethod]
+		public void GetCategoriesTest()
+		{
+			//Arrange
+			var categoryRepositoryMock = new Mock<IRepository<Category>();
+			categoryRepositoryMock.Setup(a => a.GetAll()).Returns(new List<Category>()
+
+			{
+				new Category {  Name = "Name1" },
+                new Category {  Name = "Name2" }
+
+			});
+
+			var uowMock = new Mock<IUnitOfWork>();
+			uowMock.Setup(uow => uow.Categories).Returns(categoryRepositoryMock.Object);
+
+			var service = new CategoryService(uowMock.Object);
+
+			//Act
+			var actual = service.GetCategories();
+
+			//Assert
+			Microsoft.VisualStudio.TestTools.UnitTesting.Assert.List(new List<string>) { "Все", "Gonduras", "Nigeria" }, actual);
+		}
+		*/
+	}
+
+}
+
---
+++ BllTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BllTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BllTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02839a5f-af46-49e3-89d7-6b20f6b5d6c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
